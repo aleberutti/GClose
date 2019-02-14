507373369a03eff89cac09c077002e69661e4a26
Fausto <faustofedele2013@gmail.com>
2019-02-13 22:51:19 -0300

    Merge branches 'dev5' and 'develop' of https://github.com/aleberutti/GClose into develop
    # Conflicts:
    #	app/src/main/java/com/becafe/gclose/View/EditProfileActivity.java
    #	app/src/main/java/com/becafe/gclose/View/NavigationActivity.java

--- app/src/main/java/com/becafe/gclose/View/EditProfileActivity.java
+++ app/src/main/java/com/becafe/gclose/View/EditProfileActivity.java
@@ -7,14 +7,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.becafe.gclose.R;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,12 +31,15 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     private EditText work, studies, location, desc;
+    private TextView tvWork, tvStudies, tvLoc, tvDesc;
+    private View divider;
     private MaterialButton btnPerfil, btnPortada, btnGaleria, btnGuardarCambios;
     private static final int GALLERY_INTENT=1;
     private static final int GALLERY_INTENT_PERFIL=2;
@@ -42,7 +50,9 @@ public class EditProfileActivity extends AppCompatActivity {
 
     private DatabaseReference myRef;
     private StorageReference storageRef;
+    private ProgressBar mProgressBar;
     private boolean flagPerfil;
+    public boolean flagOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +64,21 @@ public class EditProfileActivity extends AppCompatActivity {
         storageRef = FirebaseStorage.getInstance().getReference();
         flagPerfil = false;
 
+        mProgressBar = findViewById(R.id.progressbar);
+
         work = findViewById(R.id.EditWork);
         studies = findViewById(R.id.EditStudies);
         location = findViewById(R.id.EditLocation);
         desc = findViewById(R.id.EditDesc);
+
+        tvWork = findViewById(R.id.tvWork);
+        tvStudies = findViewById(R.id.tvStudies);
+        tvLoc = findViewById(R.id.tvLocation);
+        tvDesc = findViewById(R.id.tvDesc);
+
+        divider = findViewById(R.id.divider2);
+
+        flagOut = false;
 
         Bundle b = getIntent().getExtras();
         if (b!=null) {
@@ -173,12 +194,25 @@ public class EditProfileActivity extends AppCompatActivity {
             hijo = "foto_portada";
         }
 
+        Log.e("ZAF HABERFLAG2",String.valueOf(flagOut));
+        new MyTask().execute();
+        Log.e("ZAF HABERFLAG3",String.valueOf(flagOut));
+
         if(requestCode==GALLERY_INTENT && resultCode==RESULT_OK){
             Uri uri = data.getData();
             storageRef.child(mAuth.getCurrentUser().getUid()).child("images").child(hijo).putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    flagOut = true;
                     Toast.makeText(EditProfileActivity.this, "Se ha actualizado su foto", Toast.LENGTH_SHORT).show();
+                    flagOut = true;
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    flagOut = true;
+                    Toast.makeText(EditProfileActivity.this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -189,7 +223,15 @@ public class EditProfileActivity extends AppCompatActivity {
             storageRef.child(mAuth.getCurrentUser().getUid()).child("images").child(hijo).putFile(getImageUri(this, imageBitmap)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    flagOut = true;
                     Toast.makeText(EditProfileActivity.this, "Se ha actualizado su foto", Toast.LENGTH_SHORT).show();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    flagOut = true;
+                    Toast.makeText(EditProfileActivity.this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -201,6 +243,94 @@ public class EditProfileActivity extends AppCompatActivity {
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
         String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, null, null);
         return Uri.parse(path);
+    }
+
+    public boolean isFlagOut() {
+        return flagOut;
+    }
+
+    public void setFlagOut(boolean flagOut) {
+        this.flagOut = flagOut;
+    }
+
+    class MyTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+            Log.e("ZAF HABERFLAG1",String.valueOf(flagOut));
+            mProgressBar.setVisibility(View.VISIBLE);
+            work.setVisibility(View.INVISIBLE);
+            studies.setVisibility(View.INVISIBLE);
+            location.setVisibility(View.INVISIBLE);
+            desc.setVisibility(View.INVISIBLE);
+            btnPerfil.setVisibility(View.INVISIBLE);
+            btnPortada.setVisibility(View.INVISIBLE);
+            btnGaleria.setVisibility(View.INVISIBLE);
+            btnGuardarCambios.setVisibility(View.INVISIBLE);
+            tvDesc.setVisibility(View.INVISIBLE);
+            tvLoc.setVisibility(View.INVISIBLE);
+            tvStudies.setVisibility(View.INVISIBLE);
+            tvWork.setVisibility(View.INVISIBLE);
+            divider.setVisibility(View.INVISIBLE);
+
+            //Toast.makeText(getApplicationContext(), "Pongo visible", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.e("ZAF HABERFLAG4",String.valueOf(flagOut));
+            do {
+                for (int i = 0; i <= 15; i++) {
+                    Log.e("ZAF HABERFLAG",String.valueOf(flagOut));
+                    try {
+                        Log.e("ZAF HABERFLAG0",String.valueOf(flagOut));
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    publishProgress(i);
+                    if (flagOut){
+                        Log.e("ZAF HABERFLAG99",String.valueOf(flagOut));
+                        return "Fin";
+                    }
+                }
+            }while(!flagOut);
+            Log.e("ZAF HABERFLAG5",String.valueOf(flagOut));
+            return "Fin";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            mProgressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            flagOut = false;
+
+            mProgressBar.setVisibility(View.INVISIBLE);
+            work.setVisibility(View.VISIBLE);
+            studies.setVisibility(View.VISIBLE);
+            location.setVisibility(View.VISIBLE);
+            desc.setVisibility(View.VISIBLE);
+            btnPerfil.setVisibility(View.VISIBLE);
+            btnPortada.setVisibility(View.VISIBLE);
+            btnGaleria.setVisibility(View.VISIBLE);
+            btnGuardarCambios.setVisibility(View.VISIBLE);
+            tvDesc.setVisibility(View.VISIBLE);
+            tvLoc.setVisibility(View.VISIBLE);
+            tvStudies.setVisibility(View.VISIBLE);
+            tvWork.setVisibility(View.VISIBLE);
+            divider.setVisibility(View.VISIBLE);
+
+        }
     }
 
 }
